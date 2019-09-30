c17d0c43651c467e751cf0dcdee7c38f9b228b30
JAXPARROW <jacksonlinus95@gmail.com>
2019-09-30 16:57:36 +0300

    added bootstrap theme for front-end designing and templates

--- shop/inventory/views.py
+++ shop/inventory/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from inventory.models import Product
 
-# Create your views here.
+def index(request):
+    template = "inventory/index.html"
+    product_list = Product.objects.all()
+    context = {'product_list': product_list}
+    return render(request, template, context)
+    
--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -70,6 +70,7 @@ DATABASES = {
 }
 
 
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -107,3 +108,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
